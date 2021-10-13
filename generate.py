559703730a9713ac19abca1f0e3927f4c21f5dd2
Christian Knight <christianknight9@gmail.com>
2021-10-13 13:39:12 -0400

    Improve portability of main process (to call from another program)

--- generate.py
+++ generate.py
@@ -11,20 +11,12 @@
 
 import os, shutil, sys
 
-def main():
-    # Get source and destination directories from command line arguments
-    if len(sys.argv) == 3:
-        DIR_SRC = sys.argv[1]
-        DIR_DEST = sys.argv[2]
-    else:
-        print("Incorrect usage!")
-        exit()
-
+def main_process(source_dir, dest_dir):
     file_list_src = []     # for storing a list of files in the source directory
     file_list_dest = []    # for storing a list of files in the destination directory
 
     # Get a list of files already in the destination directory
-    for root, dirs, files in os.walk(DIR_DEST):
+    for root, dirs, files in os.walk(dest_dir):
         for file in files:
             if file == "Thumbs.db":
                 print(os.path.join(root, file))
@@ -35,7 +27,7 @@ def main():
                 file_list_dest.append(file)
 
     # Get a list of files in the source directory, copy to destination directory if not already present
-    for root, dirs, files in os.walk(DIR_SRC):
+    for root, dirs, files in os.walk(source_dir):
         for file in files:
             if file == "Thumbs.db":
                 print("Deleting ", os.path.join(root, file))
@@ -46,7 +38,7 @@ def main():
             if file in file_list_src and file not in file_list_dest:
                 print(file, "not in dest! Copying...")
                 path_file = os.path.join(root, file)
-                shutil.copy2(path_file, DIR_DEST)
+                shutil.copy2(path_file, dest_dir)
                 file_list_dest.append(file)
 
     # Find matches between source and destination and print list
@@ -66,13 +58,20 @@ def main():
         val1 = input("Do you want to delete files only in destination? (y/n): ")
         if val1 == 'y':
             for file in files_only_in_dest:
-                os.remove(os.path.join(DIR_DEST, file))
+                os.remove(os.path.join(dest_dir, file))
         elif val1 == 'n':
             val2 = input("Ok, do you want to copy them to the source directory? (y/n): ")
             if val2 == 'y':
                 for file in files_only_in_dest:
-                    path_file = os.path.join(DIR_DEST, file)
-                    shutil.copy2(path_file, DIR_SRC)
+                    path_file = os.path.join(dest_dir, file)
+                    shutil.copy2(path_file, source_dir)
 
 if __name__== "__main__":
-   main()
+    # Get source and destination directories from command line arguments
+    if len(sys.argv) == 3:
+        DIR_SRC = sys.argv[1]
+        DIR_DEST = sys.argv[2]
+        main_proccess(DIR_SRC, DIR_DEST)
+    else:
+        print("Incorrect usage!")
+        exit()
